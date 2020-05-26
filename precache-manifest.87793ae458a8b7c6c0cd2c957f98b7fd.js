de3ed05875c5ab1e3c1473115d993175ca837981
gaponukz <gaponukz54@hmail.com>
2020-05-26 10:40:09 +0300

    Updates

---
+++ precache-manifest.87793ae458a8b7c6c0cd2c957f98b7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7291aa6c5ee37758372490d9750529",
+    "url": "/arrange-game/index.html"
+  },
+  {
+    "revision": "85844ab552af26a69f63",
+    "url": "/arrange-game/static/css/main.36293b25.chunk.css"
+  },
+  {
+    "revision": "b7c4d6affcacbf39894c",
+    "url": "/arrange-game/static/js/2.a6404d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arrange-game/static/js/2.a6404d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85844ab552af26a69f63",
+    "url": "/arrange-game/static/js/main.262d57ef.chunk.js"
+  },
+  {
+    "revision": "12ac3f41d363b28bdc16",
+    "url": "/arrange-game/static/js/runtime-main.769ea8a9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/arrange-game/precache-manifest.87793ae458a8b7c6c0cd2c957f98b7fd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arrange-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
